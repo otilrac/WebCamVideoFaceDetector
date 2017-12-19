355d18af7ad600df94b5a14335f13eda8f5ad4c9
Vigneshwar <haivicky@gmail.com>
2017-12-19 20:08:13 +0800

    Face detection done

--- FaceDetector.py
+++ FaceDetector.py
@@ -1,40 +1,46 @@
-import numpy as np 
+import numpy as np
+import sys
+sys.path.append('/usr/local/lib/python2.7/site-packages')
 import cv2
-
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
-#cap = cv2.VideoCapture(0)
-
-#while(True):
-#ret, frame = cap.read()
-
-frame = cv2.imread('baby.png')
-
-#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#gray = cv2.equalizeHist(gray)
-
-#Detect faces
-# faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-# print len(faces)
-# #print(faces.length)
-# for (x,y,w,h) in faces:
-# 	cv2.rectangle(gray, (x,y), (x+w,y+h), (255,0,0), 2)
-
-# faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-# for (x,y,w,h) in faces:
-#     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-#     roi_gray = gray[y:y+h, x:x+w]
-#     roi_color = img[y:y+h, x:x+w]
-#     eyes = eye_cascade.detectMultiScale(roi_gray)
-#     for (ex,ey,ew,eh) in eyes:
-#         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+import matplotlib.pyplot as plt
+import time
 
 
-#frame = cv2.Canny(frame,100,200)
+def convertToRGB(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-cv2.imshow('frame', frame)
-cv2.waitKey(0)
 
-#cap.release()
+haar_face_cascade = cv2.CascadeClassifier('/Users/Vicky/Dropbox/Projects/WebCamVideoFaceDetector/classifiers/haarcascade_frontalface_alt.xml')
+img = cv2.imread('/Users/Vicky/Dropbox/Projects/WebCamVideoFaceDetector/baby.png')
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    #Detect faces
+    faces = haar_face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5);
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
+    
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
 cv2.destroyAllWindows()
+
+    
+
+
+
+    
+#print((faces))
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 255), 3)
+
+plt.imshow(img)
